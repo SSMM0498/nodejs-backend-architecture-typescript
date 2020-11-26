70a73e119c32a42728defc56fd64cd3badb07a7b
Serigne Saliou Mbacké Mbaye <barsaliou1998@gmail.com>
2020-11-26 04:11:52 +0000

    init testing tool

---
+++ src/sample.ts
@@ -0,0 +1,12 @@
+class Calculator {
+    constructor() {}
+
+    /**
+     * sum
+     */
+    public static sum(a: number,b: number):number {
+        return a + b
+    }
+}
+
+export default Calculator
